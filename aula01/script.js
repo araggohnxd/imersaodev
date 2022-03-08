7eeaafec7f6119f218347521e3eac7086ad4fed9
araggohnxd <matheusnagato300@gmail.com>
2022-03-08 16:38:30 -0300

    Refactor script.js

--- aula01/script.js
+++ aula01/script.js
@@ -23,7 +23,28 @@ function calcMean() {
 	else
 		status = "reprovado.";
 	document.querySelector("h2").innerHTML = "Sua média: " + (total / 4);
-	document.querySelector(".wrapper").innerHTML = "<h2>Você foi " + status + "</h2>";
+	document.querySelector(".wrapper").innerHTML = "<h2 id='status'>Você foi " + status + "</h2>";
 	document.querySelector("#submit").innerHTML = "Calcular nova média";
-	document.querySelector("#submit").setAttribute("onclick", "window.location.reload()");
+	document.querySelector("#submit").setAttribute("onclick", "refresh()");
 }
+
+function refresh() {
+	stepperContent();
+	document.querySelector("h2").innerHTML = "Insira suas notas";
+	document.querySelector("#status").style.display = "none";
+	document.querySelector("#submit").innerHTML = "Calcular média";
+	document.querySelector("#submit").setAttribute("onclick", "calcMean()");
+}
+
+function stepperContent() {
+	for (let i = 1; i <= 4; i++) {
+			document.querySelector(".wrapper").innerHTML += `
+			<div class="stepper">
+				<button class="button" id="increment" onclick="stepper(this, '${i}')">⮝</button>
+				<input class="grade" id="number${i}" type="number" min="0" max="10" value="0" step="1" readonly>
+				<button class="button" id="decrement" onclick="stepper(this, '${i}')">⮟</button>
+			</div>`;
+	}
+}
+
+window.onload = stepperContent();
